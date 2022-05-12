e05b812bf47950ba00353580c446e9cb1b642a4d
jiyeon0116 <gbko159@naver.com>
2022-05-12 20:06:28 +0900

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,5 +1,10 @@
-from dice import *
+<<<<<<< HEAD
 
+
+=======
+from Dice import *
+import csv
+>>>>>>> efa9e63aa240c1b117d262074ab6cf0bcbd20b14
 def select_agent(player):
     agent_list = []
 
@@ -27,11 +32,57 @@ class Player:
     def select_kt(self, kt):
         self.kt = Killteam(kt) 
 
+    def select_ft(self):
+        ft_list = self.get_ft_csv()
+        for i in range(len(ft_list)):
+            text = str(i+1) + ": " + ft_list[i][2]
+            print(text, end=" ")
+
+        print("\n파이어팀1 선택: ", end ="")
+        user_input = int(input())
+
+        selected_ft = ft_list[user_input-1][2]
+        ft1_archetypes = ft_list[user_input-1][3:]
+
+        self.select_ft1(selected_ft)
+
+        print("파이어팀2 선택: ", end ="")
+        user_input = int(input())
+
+        selected_ft = ft_list[user_input-1][2]
+        ft2_archetypes = ft_list[user_input-1][3:]
+
+        self.select_ft2(selected_ft)
+        print(selected_ft)
+
+        self.ft1.set_archetypes(ft1_archetypes)
+        self.ft2.set_archetypes(ft2_archetypes)
+
     def select_ft1(self, ft):
         self.ft1 = Fireteam(ft)
+        print("선택된 파이어팀: " + self.ft1.ft_type)
 
     def select_ft2(self, ft):
         self.ft2 = Fireteam(ft)
+        print("선택된 파이어팀: " + self.ft2.ft_type)
+
+    def get_ft_csv(self):
+        file = open('game_data/fireTeams.csv')
+        type(file)
+        csvreader = csv.reader(file)
+
+        header = []
+        header = next(csvreader)
+        header
+
+        rows = []
+        for row in csvreader:
+                if row[1] == self.kt.type:
+                    rows.append(row)
+
+        file.close()
+
+        return rows
 
 class Killteam:
     def __init__(self, kt):
@@ -175,19 +226,20 @@ class Agent:
 
     def fight(self, opponent):
         target = select_agent(opponent) # 상대 player로부터 공격 대상 선택
-        weapon = self.select_weapon("Melee") # Melee 타입 무기 선택
+        attacker_weapon = self.select_weapon("Melee") # 공격자 Melee 타입 무기 선택
+        defender_weapon = target.select_weapon("Melee") # 수비자 Melee 타입 무기 선택
 
-        dice_result = dice(weapon.a) # 선택한 무기의 a스탯만큼 주사위 굴림
+        dice_result = dice(attacker_weapon.a) # 선택한 무기의 a스탯만큼 주사위 굴림
 
         attacker_normal, attacker_crit, attacker_miss = 0, 0, 0
         for i in dice_result:
             if i == 6:                # 주사위 6나올 경우 치명타
                 attacker_crit += 1
-            elif i >= weapon.ws:      # 무기의 ws 수치보다 높을 경우 일반 히트
+            elif i >= attacker_weapon.ws:      # 공격자 무기의 ws 수치보다 높을 경우 일반 히트
                 attacker_normal += 1
             else:
                 attacker_miss += 1
-        print("공격자 타격성공: %2d(평타: %2d, 치명타: %2d), 타격실패: %2d" %(attacker_normal+attacker_crit, attacker_normal, attacker_crit, attacker_miss))
+        print("공격자 공격성공: %2d(일반공격: %2d, 치명공격: %2d), 공격실패: %2d" %(attacker_normal+attacker_crit, attacker_normal, attacker_crit, attacker_miss))
 
         dice_result = dice(target.df) # 수비하는 요원의 df수치 만큼 주사위 굴림
 
@@ -195,11 +247,138 @@ class Agent:
         for i in dice_result:
             if i == 6:
                 defender_crit += 1        # 주사위 6나올 경우 크리티컬 수비
-            elif i >= target.sv:
-                defender_normal += 1             # 요원의 sv 수치보다 높을 경우 일반 수비
+            elif i >= defender_weapon.ws:
+                defender_normal += 1             # 수비자 무기의 ws 수치보다 높을 경우 일반 수비
             else:
                 defender_attacker_miss += 1
         print("수비성공: %2d(일반수비: %2d, 치명수비: %2d), 수비실패: %2d" %(defender_normal+defender_crit, defender_normal, defender_crit, defender_attacker_miss))
+
+        for i in range(attacker_normal+attacker_crit+defender_normal+defender_crit):
+            turn = 0
+            if (turn%2 == 0 or defender_normal+defender_crit == 0) and not attacker_normal + attacker_crit == 0:
+                while True:
+                    print("공격자 턴 / 일반주사위(1): %2d, 치명주사위(2): %d 선택: "%(attacker_normal, attacker_crit), end="")
+                    dice_choice = int(input())
+                    if dice_choice == 1:
+                        if attacker_normal == 0:
+                            print("사용할 주사위 없음")
+                        else:
+                            if defender_normal > 0:
+                                print("공격(1) 혹은 방어(2) 선택: ",end="")
+                                strike_or_parry = int(input())
+                            else:
+                                print("상대방 일반수비 주사위가 없어 바로 공격 처리")
+                                strike_or_parry = 1
+
+                            if strike_or_parry == 1:
+                                print("기존 상대 요원 체력 %2d에서 " %target.w, end="")
+                                target.w -= attacker_weapon.d
+                                print("공격 무기 데미지 %2d만큼 감소되어 상대 요원 체력이 %2d로 바뀜" %(attacker_weapon.d, target.w))
+                            elif strike_or_parry == 2:
+                                print("기존 수비자 요원 기본주사위 %2d에서 " %defender_normal)
+                                defender_normal -= 1
+                                print("1만큼 감소 되어 %2d로 바뀜" %defender_normal)
+                                i += 1
+
+                            attacker_normal -= 1
+
+                            turn += 1
+                            break
+
+                    if dice_choice == 2:
+                        if attacker_crit == 0:
+                            print("사용할 주사위 없음")
+                        else:
+                            if defender_normal + defender_crit > 0:
+                                print("공격(1) 혹은 방어(2) 선택: ",end="")
+                                strike_or_parry = int(input())
+                            else:
+                                print("상대방 일반수비 주사위가 없어 바로 공격 처리")
+                                strike_or_parry = 1
+
+                            if strike_or_parry == 1:
+                                print("기존 상대 요원 체력 %2d에서 " %target.w, end="")
+                                target.w -= attacker_weapon.d_crit
+                                print("공격 무기 치명 데미지 %2d만큼 감소되어 상대 요원 체력이 %2d로 바뀜" %(attacker_weapon.d_crit, target.w))
+                            elif strike_or_parry == 2:
+                                if defender_crit > 0:
+                                    print("기존 수비자 요원 치명주사위 %2d에서 " %defender_crit)
+                                    defender_crit -= 1
+                                    print("1만큼 감소 되어 %2d로 바뀜" %defender_crit)
+                                    i += 1
+                                else:
+                                    print("기존 수비자 요원 치명주사위 %2d에서 " %defender_normal)
+                                    defender_normal -= 1
+                                    print("1만큼 감소 되어 %2d로 바뀜" %defender_normal)
+                                    i += 1
+
+                            attacker_crit -= 1
+                            
+                            turn += 1
+                            break
+
+
+            if (turn%2 == 1 or attacker_normal+attacker_crit == 0) and not defender_normal + defender_crit == 0:
+                while True:
+                    print("수비자 턴 / 일반주사위(1): %2d, 치명주사위(2): %d 선택: "%(defender_normal, defender_crit), end="")
+                    dice_choice = int(input())
+                    if dice_choice == 1:
+                        if defender_normal == 0:
+                            print("사용할 주사위 없음")
+                        else:
+                            if attacker_normal > 0:
+                                print("공격(1) 혹은 방어(2) 선택: ",end="")
+                                strike_or_parry = int(input())
+                            else:
+                                print("상대방 일반수비 주사위가 없어 바로 공격 처리")
+                                strike_or_parry == 1
+
+                            if strike_or_parry == 1:
+                                print("기존 상대 요원 체력 %2d에서 " %self.w, end="")
+                                self.w -= defender_weapon.d
+                                print("공격 무기 데미지 %2d만큼 감소되어 상대 요원 체력이 %2d로 바뀜" %(defender_weapon.d, self.w))
+                            elif strike_or_parry == 2:
+                                print("기존 공격자 요원 기본주사위 %2d에서 " %attacker_normal)
+                                attacker_normal -= 1
+                                print("1만큼 감소 되어 %2d로 바뀜" %attacker_normal)
+                                i += 1
+
+                            defender_normal -= 1
+
+                            turn += 1
+                            break
+
+                    if dice_choice == 2:
+                        if defender_crit == 0:
+                            print("사용할 주사위 없음")
+                        else:
+                            if attacker_normal + attacker_crit > 0:
+                                print("공격(1) 혹은 방어(2) 선택: ",end="")
+                                strike_or_parry = int(input())
+                            else:
+                                print("상대방 일반수비 주사위가 없어 바로 공격 처리")
+                                strike_or_parry == 1
+
+                            if strike_or_parry == 1:
+                                print("기존 상대 요원 체력 %2d에서 " %self.w, end="")
+                                self.w -= defender_weapon.d_crit
+                                print("공격 무기 치명 데미지 %2d만큼 감소되어 상대 요원 체력이 %2d로 바뀜" %(defender_weapon.d_crit, self.w))
+                            elif strike_or_parry == 2:
+                                if attacker_crit > 0:
+                                    print("기존 공격자 요원 치명주사위 %2d에서 " %attacker_crit)
+                                    attacker_crit -= 1
+                                    print("1만큼 감소 되어 %2d로 바뀜" %attacker_crit)
+                                    i += 1
+                                else:
+                                    print("기존 공격자 요원 기본주사위 %2d에서 " %attacker_normal)
+                                    attacker_normal -= 1
+                                    print("1만큼 감소 되어 %2d로 바뀜" %attacker_normal)
+                                    i += 1
+
+                            defender_crit -= 1
+                            
+                            turn += 1
+                            break
 
 class Weapon:
     def __init__(self, weapon_name, type, variant = "default"):
